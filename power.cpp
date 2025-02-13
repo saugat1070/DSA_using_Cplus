c0402713921127f8bee73dddead3e3b13eea1e08
saugat1070 <saugatgiri1070@gmail.com>
2025-02-13 14:50:28 +0545

    adding to find pow() function using recursion

---
+++ power.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+double pow(double x, int n){
+    if(n == 0){ return 1.00;}
+    if (n > 0){
+        return x * pow(x,n-1);
+    }
+    return 1.0 / pow(x,-n);
+    
+    
+}
+int main(){
+    cout.precision(5);
+    cout << fixed << pow(8.88023, 3)<< endl;
+    cout<<endl;
+}
